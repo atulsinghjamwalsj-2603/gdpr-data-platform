f4bb3279bb1e7f40a68d496045b4fdc429ed70e2
Atul Singh Jamwal <atulsinghjamwal.sj@gmail.com>
2026-05-01 13:23:23 +0530

    Add User entity to domain layer

---
+++ Gdpr.Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+namespace Gdpr.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+
+    public string Email { get; private set; }
+
+    public string FullName { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+
+    public DateTime CreatedAt { get; private set; }
+
+    private User() { } // For ORM later
+
+    public User(string email, string fullName)
+    {
+        Id = Guid.NewGuid();
+        Email = email;
+        FullName = fullName;
+        CreatedAt = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+
+    public void MarkAsDeleted()
+    {
+        IsDeleted = true;
+    }
+}
